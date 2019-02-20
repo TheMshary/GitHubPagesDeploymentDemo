f4524239602fe0fef9b522a42ec814acc57b1770
Mshary <m.alsharekh94@gmail.com>
2019-02-20 13:20:31 +0300

    Add files via upload

---
+++ static/js/index.js
@@ -0,0 +1,3 @@
+function clickme() {
+	alert("You have successfully clicked the ClickMe button! Bravo! Tap yourself on the shoulder, you're brilliant. Don't be ashamed of that smile, you've earned it!")
+}
